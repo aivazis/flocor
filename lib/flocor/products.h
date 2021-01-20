f70621bd9e1ad7848407584fad206270d3c5c3bd
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-01-20 11:56:22 +0200

    flocor: primed the library

---
+++ lib/flocor/products.h
@@ -0,0 +1,17 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+// code guard
+#if !defined(flocor_products_h)
+#define
+
+
+// publish the product specs
+#include "products/public.h"
+
+
+#endif
+
+// end of file
---
+++ lib/flocor/products/forward.h
@@ -0,0 +1,13 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+// code guard
+#if !defined(flocor_products_public_h)
+#define
+
+
+#endif
+
+// end of file
