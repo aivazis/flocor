f07a24e62b8cd540372cc14034b2bf8cd07107ae
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-02-01 23:47:07 +0200

    ux/views: the {flo2d} panel now runs a simple query when it mounts

--- ux/client/views/flo2d/index.js
+++ ux/client/views/flo2d/index.js
@@ -6,7 +6,9 @@
 
 // externals
 import React from 'react'
+import { graphql, useFragment, useLazyLoadQuery } from 'react-relay/hooks'
 // locals
+import { Compass } from '~/widgets'
 import styles from './styles'
 
 
@@ -15,10 +17,28 @@ const Panel = () => {
     // make a reference for the section
     const viewRef = React.useRef(null)
 
+    // load a query
+    const data = useLazyLoadQuery(
+        graphql`
+            query flo2dQuery {
+                catalog {
+                    producers {
+                        family
+                    }
+                    specifications {
+                        family
+                    }
+                }
+            }`
+    )
+
     // build the container and return it
     return (
         <section ref={viewRef} style={styles.panel} >
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" {...styles.canvas}>
+                <g transform="translate(200 200) scale(100)" >
+                    <Compass />
+                </g>
             </svg>
         </section>
     )
