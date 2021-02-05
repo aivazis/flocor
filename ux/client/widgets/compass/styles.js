e338b7af843eb71edc9de9e4666463cb5c17255e
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-02-05 09:22:09 +0200

    flocor/ux: updated the {compass} attributions

--- ux/client/widgets/compass/styles.js
+++ ux/client/widgets/compass/styles.js
@@ -1,7 +1,7 @@
 // -*- web -*-
 //
-// michael a.g. aïvázis <michael@aivazis.com>
-// (c) 2020-2021 all rights reserved
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
 
 
 // get colors
