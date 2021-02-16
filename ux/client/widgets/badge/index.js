069cdc1dac06bd59fba25a83e9841eb41c070b33
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-02-16 10:46:26 -0800

    ux/widgets: added {badge}, a container of an svg {shape} that will eventually be a button

---
+++ ux/client/widgets/badge/index.js
@@ -0,0 +1,71 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// externals
+import React from 'react'
+// locals
+import styles from './styles'
+
+
+// a button with an SVG image as content
+const badge = ({ size=32, style, children }) => {
+    // mix my paint
+    // for the container
+    const badgeStyle = {
+        ...style?.badge, ...styles.badge,
+        width: size,
+        height: size,
+    }
+    // and the shape
+    const shapeStyle = { ...style?.shape, ...styles.shape }
+
+    // make a ref for the shape container
+    const ref = React.useRef()
+
+    // state controls
+    // the default state
+    const normal = () => {
+        // get the style of the shape container
+        const style = ref.current.style
+        // style it
+        style.fillOpacity = 0.5
+        style.strokeOpacity = 0.5
+    }
+    // when the shape is highlighted
+    const emph = () => {
+        // get the style of the shape container
+        const style = ref.current
+        // style it
+        style.fillOpacity = 1
+        style.strokeOpacity = 1
+    }
+    // map the controls to mouse events
+    const controls = {
+        onMouseEnter: emph,
+        onMouseLeave: normal,
+    }
+
+    // scale to size, assuming the shape is drawn in a 1k^2 box
+    const shrink = `scale(${size/1000})`
+
+    // paint me
+    return (
+        <div style={badgeStyle} {...controls} >
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width={size} height={size} >
+                <g ref={ref} transform={shrink} style={shapeStyle} >
+                    {children}
+                </g>
+            </svg>
+        </div>
+    )
+}
+
+
+// publish
+export default badge
+
+
+// end of file
---
+++ ux/client/widgets/badge/styles.js
@@ -0,0 +1,26 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// get colors
+import { theme } from '~/palette'
+
+
+// publish
+export default {
+    // the container
+    badge: {
+        margin: "0.75rem",
+    },
+
+    // the shape
+    shape: {
+        fillOpacity: 0.5,
+        strokeOpacity: 0.5,
+    },
+}
+
+
+// end of file
