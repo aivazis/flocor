e0066446a6835cb32aa7734ed49d935ba3fef252
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-02-16 21:39:52 -0800

    ux/activities: added {viz} that grants access to the visualization engine

---
+++ ux/client/activities/viz/index.js
@@ -0,0 +1,43 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// externals
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// locals
+// widgets
+import { Activity } from '~/activities'
+// my shape
+import { Eye } from '~/shapes'
+// styles
+import styles from './styles'
+
+
+// visualize
+const activity = ({ size=32, style }) => {
+    // mix my paint
+    const activityStyle = {
+        // for the badge
+        badge: { ...styles.badge, ...style?.badge },
+        // for the shape
+        shape: { ...styles.shape, ...style?.shape },
+    }
+
+    // paint me
+    return (
+        <Activity size={size} url="/visualize" style={activityStyle} >
+            <Eye />
+        </Activity >
+    )
+}
+
+
+// publish
+export default activity
+
+
+// end of file
