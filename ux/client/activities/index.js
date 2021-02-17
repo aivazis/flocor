0bed4f01d1c526b9e70be617d7b389a2cdaeca7c
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-02-16 21:40:21 -0800

    ux/activities: published the newly added activities

---
+++ ux/client/activities/index.js
@@ -0,0 +1,21 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// publish
+// the base component
+export Activity from './activity'
+// activities
+export About from './about'
+export Archive from './archive'
+export Compose from './compose'
+export Deploy from './deploy'
+export Experiment from './experiment'
+export Help from './help'
+export Kill from './kill'
+export Visualize from './viz'
+
+
+// end of file
