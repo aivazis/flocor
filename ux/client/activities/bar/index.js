5e892a7953d14ca7b5a9d93b0e69df278573b75b
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-02-17 13:10:11 -0800

    ux/activities: added the activity {bar} with the full set of activity placeholders

---
+++ ux/client/activities/bar/index.js
@@ -0,0 +1,47 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// locals
+// the activities
+import { About, Archive, Compose, Deploy, Experiment, Help, Kill, Visualize } from '~/activities'
+// widgets
+import { Toolbar, Spacer } from '~/widgets'
+// styles
+import styles from './styles'
+
+
+// teh activity bar
+const bar = () => {
+    // decide on a size
+    const size = 32
+
+    // paint me
+    return (
+        <Toolbar direction="column">
+            <Compose size={size} style={styles} />
+            <Deploy size={size} style={styles} />
+            <Visualize size={size} style={styles} />
+            <Archive size={size} style={styles} />
+            <Help size={size} style={styles} />
+            <Experiment size={size} style={styles} />
+
+            <Spacer />
+
+            <Kill size={size} style={styles} />
+            <About size={size} style={styles} />
+        </Toolbar>
+    )
+}
+
+
+// publish
+export default bar
+
+
+// end of file
