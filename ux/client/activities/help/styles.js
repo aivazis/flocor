88d16b1cc449660c4a71f1569aed3e551f1d3cdb
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-02-16 21:38:33 -0800

    ux/activities: added {help} to get access to the online documentation

---
+++ ux/client/activities/help/styles.js
@@ -0,0 +1,24 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// get colors
+import { theme } from '~/palette'
+
+
+// publish
+export default {
+
+    // styling for the badge
+    badge: {
+    },
+
+    // styling for the shape
+    shape: {
+    },
+}
+
+
+// end of file
