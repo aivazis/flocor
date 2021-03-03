cfc358dd99ac43af9f913a59fc9eefb24b57d621
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-03-03 09:32:18 -0800

    ux/activities: restyled the bar to look more like {vscode}

--- ux/client/activities/bar/styles.js
+++ ux/client/activities/bar/styles.js
@@ -12,6 +12,8 @@ import { theme } from '~/palette'
 export default {
     // for my toolbar
     box: {
+        // style
+        padding: "0.5rem 0.0rem",
         // paint
         backgroundColor: "hsl(0deg, 0%, 21%, 1)",
     },
@@ -19,7 +21,7 @@ export default {
     // my opinions on badges
     badge: {
         //  styling
-        padding: "0.75rem",
+        padding: "0.5rem 0.5rem",
         // make a transparent border of the correctwidth so the badges don't move around
         // when the corresponding activity is engaged
         borderLeft: `2px solid hsl(0deg, 0%, 0%, 0)`,
