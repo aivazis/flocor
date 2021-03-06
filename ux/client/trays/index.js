8c90a954769d0ce7e88b927389d8186f345c0062
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-03-06 11:41:06 -0800

    ux/trays: published {node} and {tray}

---
+++ ux/client/trays/index.js
@@ -0,0 +1,15 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// publish
+
+// the base node
+export Node from './node'
+// the base tray
+export Tray from './tray'
+
+
+// end of file
