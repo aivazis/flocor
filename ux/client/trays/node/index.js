1d9ca83958269d811ed4a12b49b26c0395693cc2
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-03-11 14:07:40 -0800

    ux/trays: the generic tray {node} now updates the {flo2d} context with the details of the correct cursor decoration to use while dragging

--- ux/client/trays/node/index.js
+++ ux/client/trays/node/index.js
@@ -8,21 +8,39 @@
 import React from 'react'
 
 // locals
+// context
+import useShadow from '~/views/flo2d/useShadow'
+import useShadowOffset from '~/views/flo2d/useShadowOffset'
 // styles
 import styles from './styles'
 
 
 // a tray with a single flow node
-const node = ({ family, shape, style }) => {
+const node = ({ family, shape, size, style }) => {
     // my current state
     const [state, setState] = React.useState("normal")
+    // make a callback that sets the cursor shadow when initiating node drag
+    const { attachShadow } = useShadow({ shape })
+    // make a call back that shifts the cursor shadow so it's visible
+    const { attachShadowOffset } = useShadowOffset({ offset: { x: -size.x, y: -size.y } })
 
+    // to select me
+    const select = () => {
+        // attach the cursor shadow
+        attachShadow()
+        // and its offset
+        attachShadowOffset()
+        // all done
+        return
+
+    }
     // to turn highlighting on
     const highlightOn = () => setState("highlighted")
     // to turn highlighting off
     const highlightOff = () => setState("normal")
     // box controls
     const boxControls = {
+        onMouseDown: select,
         onMouseEnter: highlightOn,
         onMouseLeave: highlightOff,
     }
