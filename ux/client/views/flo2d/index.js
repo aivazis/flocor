6d978e2be65daee2a2effb15e1a740197161a7fb
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-03-11 14:12:35 -0800

    ux/views: the {flo2d} view now decorates the cursor while dragging flow {node} entries from their {tray}

--- ux/client/views/flo2d/index.js
+++ ux/client/views/flo2d/index.js
@@ -11,9 +11,14 @@ import React from 'react'
 import { Switch, Route, useRouteMatch } from 'react-router-dom'
 
 // locals
+// hooks
+import { useEvent } from '~/hooks'
 import styles from './styles'
 // my context
 import { Provider } from './context'
+import useShadow from './useShadow'
+import useShadowOffset from './useShadowOffset'
+import useCursorLocation from './useCursorLocation'
 // sidebars
 import { NodeLibrary } from '~/views'
 // canvases
@@ -21,18 +26,50 @@ import { NYI } from '~/views'
 // activities
 import { ActivityBar } from '~/activities'
 // widgets
-import { Flex } from '~/widgets'
+import { Flex, Shadow } from '~/widgets'
 
 
 // the main app working area
 // the layout is simple: the activity bar and activity dependent routing
 const Panel = () => {
+    // make a ref for my top level element
+    const ref = React.useRef(null)
     // get the current activity
     const match = useRouteMatch()
+    // make a callback that resets the cursor shadow when the mouse is released
+    const { shadow, attachShadow: clearShadow } = useShadow({ shadow: null })
+    // keep track of the cursor location
+    const { cursorLocation, trackCursorLocation } = useCursorLocation()
+    // and the current shadow offset
+    const { shadowOffset } = useShadowOffset({ x: 0, y: 0 })
+
+    // install the event listeners
+    // track the location of the cursor while the mouse is moving
+    useEvent({
+        name: "mousemove", listener: trackCursorLocation, client: ref,
+        triggers: [cursorLocation, shadowOffset]
+    })
+    // clear the cursor shadow when the mouse is released
+    useEvent({
+        name: "mouseup", listener: clearShadow, client: ref,
+        triggers: [shadow]
+    })
+    // also, clear the cursor shadow when the mouse leaves my client area
+    useEvent({
+        name: "mouseleave", listener: clearShadow, client: ref,
+        triggers: [shadow]
+    })
+
+    // the cursor shadow
+    const cursorShadow = (
+        shadow
+            ? <Shadow location={cursorLocation} offset={shadowOffset} >{shadow}</Shadow>
+            : null
+    )
 
     // lay out the main page
     return (
-        <section style={styles.panel} >
+        <section ref={ref} style={styles.panel} >
             {/* navigation bar */}
             <ActivityBar style={styles.activitybar} />
 
@@ -54,6 +91,9 @@ const Panel = () => {
                 </Flex.Panel>
 
             </Flex.Box>
+
+            {/* if there is a shadow, place a shape at the cursor location */}
+            {cursorShadow}
         </section >
     )
 }
