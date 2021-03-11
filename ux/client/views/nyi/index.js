289788b232a75b4aad618aba4ecbc4044b9e7182
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-03-11 14:17:46 -0800

    ux/view: cosmetics and narrative improvements in {nyi}

--- ux/client/views/nyi/index.js
+++ ux/client/views/nyi/index.js
@@ -14,20 +14,21 @@ import styles from './styles'
 
 
 // the area
-const nyi = () => (
-    <section style={styles.loading}>
-        <div style={styles.placeholder}>
-            <svg style={styles.icon} version="1.1" xmlns="http://www.w3.org/2000/svg">
-                <g transform="scale(0.3)" fill="#f37f19" stroke="none">
-                    <Hammer style={styles.shape} />
-                </g>
-            </svg>
-            <p style={styles.message}>
-                this page is not implemented yet
-            </p>
-        </div>
-    </section>
-)
+const nyi = () => {
+    // paint a view that shows an under construction message
+    return (
+        <section style={styles.loading}>
+            <div style={styles.placeholder}>
+                <svg style={styles.icon} version="1.1" xmlns="http://www.w3.org/2000/svg">
+                    <g transform="scale(0.3)" fill="#f37f19" stroke="none">
+                        <Hammer style={styles.shape} />
+                    </g>
+                </svg>
+                <p style={styles.message}>this page is not implemented yet</p>
+            </div>
+        </section>
+    )
+}
 
 
 // publish
