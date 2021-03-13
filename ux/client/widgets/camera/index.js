cf2ffaec047723c66ca4f8a84f33a2bddde59572
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-03-13 15:19:14 -0800

    ux/widgets: published the {camera}

---
+++ ux/client/widgets/camera/index.js
@@ -0,0 +1,11 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// publish the component
+export { camera as Camera } from './camera'
+
+
+// end of file
