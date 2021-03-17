87574817ad0cae7d1c6ec89aed368ee37a0b184c
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-03-17 09:37:33 -0700

    ux/views: retired the state used to track the cursor lcoation from the {flo2d} context

--- ux/client/views/flo2d/context.js
+++ ux/client/views/flo2d/context.js
@@ -24,8 +24,6 @@ export const Provider = ({ children }) => {
     const [shadow, setShadow] = React.useState(null)
     // and its offset relative to the cursor position
     const [shadowOffset, setShadowOffset] = React.useState({ x: 0, y: 0 })
-    // to keep track of the location of the mouse
-    const [cursorLocation, setCursorLocation] = React.useState(null)
 
     // build the current value of the context
     const context = {
@@ -33,8 +31,6 @@ export const Provider = ({ children }) => {
         shadow, setShadow,
         // and its offset
         shadowOffset, setShadowOffset,
-        // cursor location
-        cursorLocation, setCursorLocation,
     }
 
     // provide for my children
