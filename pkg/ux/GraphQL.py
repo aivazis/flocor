37c2e518da2ec822c44c1723982cee9dde90d93a
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-03-19 18:19:58 -0700

    flocor.ux: added more resolution {context}; improved thehandling and display of resolution errors

--- pkg/ux/GraphQL.py
+++ pkg/ux/GraphQL.py
@@ -6,6 +6,7 @@
 
 # externals
 import graphene
+import journal
 import json
 # support
 import flocor
@@ -28,17 +29,33 @@ class GraphQL:
         # operation = payload.get("operation")
         # variables = payload.get("variables")
 
+        # get my context and make a copy of it
+        context = dict(self.context)
+        # place additional information in it
+        context["plexus"] = plexus
+        context["server"] = server
+        context["request"] = request
+
         # execute the query
-        result = self.schema.execute(query, context=self.context)
+        result = self.schema.execute(query, context=context)
 
         # assemble the resulting document
         doc = { "data": result.data }
         # in addition, if something went wrong
         if result.errors:
-            # inform the client
+            # inform the client by placing the error messages in the resulting payload
             doc["errors"] = [ {"message": error.message} for error in result.errors ]
 
-        # encode it using JSON and serve it
+            # grab my channel
+            channel = self.error
+            # and generate a report for my console by going through the errors
+            for error in result.errors:
+                # and reporting each one
+                channel.line(error.message)
+            # flush
+            channel.log()
+
+        # encode the query result using JSON and serve it
         return server.documents.JSON(server=server, value=doc)
 
 
@@ -52,8 +69,15 @@ class GraphQL:
 
         # set up the execution context
         self.context = {
+            "panel": panel,
             "nameserver": panel.pyre_nameserver,
         }
+
+        # make an error channel for my query errors
+        self.error = journal.error("flocor.gql.graphql")
+        # make sure it's not fatal; skip for now so i don't have to kill
+        # the server manually during debugging
+        # self.error.fatal = False
 
         # all done
         return
