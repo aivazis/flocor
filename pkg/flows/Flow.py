4d250ae8eb69843b7ce26b6998183d26f886d5a8
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-03-20 13:49:29 -0700

    flocor.flows: a {flow} can now contain macros

--- pkg/flows/Flow.py
+++ pkg/flows/Flow.py
@@ -17,12 +17,52 @@ class Flow(flocor.flow.workflow, family="flocor.flows.flow", implements=flocor.s
     """
 
 
+    def addNode(self, node):
+        """
+        Add a new {node} to the set
+        """
+        # easy enough
+        self.nodes[node.pyre_id] = node
+        # all done
+        return
+
+
+    def nameNode(self, node):
+        """
+        Attach a name to {node}
+        """
+        # make an entry for {node} in my {macros
+        self.macros[node.pyre_name] = node
+        # all done
+        return
+
+
+    def removeNode(self, node):
+        """
+        Remove {node} from the environment
+        """
+        # discard from the node pile
+        del self.nodes[node]
+        # and if it has a name
+        if node.pyre_name in self.macros:
+            # remove it from there as well
+            del self.macros[node.pyre_name]
+        # all done
+        return
+
+
     # metamethods
     def __init__(self, **kwds):
         # chain up
         super().__init__(**kwds)
-        # set the uuid
+        # set my uuid
         self.pyre_id = uuid.uuid1()
+
+        # initialize the set of nodes
+        self.nodes = {}
+        # initialize my node model, i.e the set of named nodes
+        self.macros = {}
+
         # all done
         return
 
