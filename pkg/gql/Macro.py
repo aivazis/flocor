627ed5f6a91b1863864331cb779b4fda5fad266a
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-03-21 11:51:21 -0700

    flocor.gql: {macro} now has a {position}

--- pkg/gql/Macro.py
+++ pkg/gql/Macro.py
@@ -7,6 +7,9 @@
 # externals
 import graphene
 
+# local types
+from .Position import Position
+
 
 # a flow node that represents a typed+named variable
 class Macro(graphene.ObjectType):
@@ -19,6 +22,9 @@ class Macro(graphene.ObjectType):
     id = graphene.ID()
     name = graphene.String(required=True)
     family = graphene.String(required=True)
+
+    # representation
+    position = graphene.Field(Position, required=True)
 
 
 # end of file
