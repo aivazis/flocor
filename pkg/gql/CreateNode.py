e470d066bbc90f9bddefe74f8616d884a55ba053
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-03-23 12:59:57 -0700

    flocor.gql: the {createNode} mutation now can add a {macro} to the {flow}

--- pkg/gql/CreateNode.py
+++ pkg/gql/CreateNode.py
@@ -3,9 +3,10 @@
 # michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2021 all rights reserved
 
-
 # externals
 import graphene
+# package
+import flocor
 # local types
 from .Position import Position
 from .NodeInfoInput import NodeInfoInput
@@ -14,12 +15,14 @@ from .NodeInfoInput import NodeInfoInput
 # a mutation that adds a new node to the flow
 class CreateNode(graphene.Mutation):
     """
-    A mutation that add a new node to the flow
+    A mutation that adds a new node to the flow
     """
+
 
     # inputs
     class Arguments:
         nodeinfo = NodeInfoInput(required=True)
+
 
     # fields
     id = graphene.ID()
@@ -27,10 +30,33 @@ class CreateNode(graphene.Mutation):
 
 
     def mutate(root, info, nodeinfo):
-        # show me
-        print(nodeinfo)
-        # make a node and return it
-        return CreateNode(id="foofoo", position=Position(x=0, y=0))
+        # unpack the node info
+        category = nodeinfo["category"]
+        family = nodeinfo["family"]
+        x = nodeinfo["x"]
+        y = nodeinfo["y"]
+
+        # get the panel
+        panel = info.context["panel"]
+        # so we can grab the {flow}
+        flow = panel.flow
+        # and its {layout}
+        layout = panel.layout
+
+        # figure out what kind of node we are creating
+        # some markers indicate a {calc} node
+        if category in ["basic", "composite", "host"]:
+            # make a {macro}; we don't have a name for it yet
+            macro = flocor.flows.macro(family=family)
+            # add it to the flow
+            flow.addNode(node=macro)
+            # and the layout
+            layout[macro.pyre_id] = {"x": x, "y": y}
+            # make a node and return it
+            return CreateNode(id=macro.pyre_id, position=Position(x=x, y=y))
+
+        # if we get this far, there is something wrong
+        raise Exception("unknown node category")
 
 
 # end of file
