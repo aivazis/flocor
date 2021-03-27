76dace7ff2bc2c492e5b835b1c6e7727646b5997
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-03-27 13:06:11 -0700

    ux/widgets: the {useCamera} hook now pull the {camera} state out of its {context}

--- ux/client/widgets/camera/useCamera.js
+++ ux/client/widgets/camera/useCamera.js
@@ -8,34 +8,16 @@
 import React from 'react'
 
 // locals
-// camera actions
-import { dispatcher, newCamera } from './reducer'
-// event listeners
-import { keypad } from './keypad'
-import { wheel } from './wheel'
-// user interaction
-import { useKeypad } from './useKeypad'
-import { useWheel } from './useWheel'
+// context
+import { Context } from './context'
 
-// {camera} is the manager of the user's point of view
-// this is a 3-axis zooming camera:
-//
-// - it can pan on the x-y plane
-// - it can zoom along the z-axis
-// - it can rotate about the z axis
-//
-// the units of these settings are up to the client
-export const useCamera = (client, init = undefined) => {
-    // initialize a camera and its controls and make them available
-    const [camera, remote] = React.useReducer(dispatcher, newCamera(), init)
 
-    // install the {keypad} bindings to the top level window
-    useKeypad(keypad(remote))
-    // and the {wheel} bindings to my view
-    useWheel(wheel(remote), client)
-
-    // publish the camera
-    return camera
+// publish the current camera state
+export const useCamera = () => {
+    // grab the camera and publish it
+    const { camera } = React.useContext(Context)
+    // and publish it
+    return { camera }
 }
 
 
