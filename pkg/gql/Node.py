194ff51b118f8471c7d54474d6a7eba81dfd39e8
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-03-29 13:31:02 -0700

    flocor.gql: the {node} interface now derives from {graphene.relay.Node} to better conform to the {relay} spec; the type map triggers a {firewall} until the execution context is understood

--- pkg/gql/Node.py
+++ pkg/gql/Node.py
@@ -12,19 +12,63 @@ from .Position import Position
 
 
 # a flow node that represents a typed+named variable
-class Node(graphene.Interface):
+class Node(graphene.relay.Node):
     """
     Specification for a base flow node
     """
 
+    # register this as the {graphene.relay.Node} replacement
+    class Meta:
+        name = "Node"
 
-    # the fields
+
+    # the basic fields
     id = graphene.ID()
     name = graphene.String(required=False)
     family = graphene.String(required=True)
-
     # representation
     position = graphene.Field(Position, required=True)
+
+
+    #  {graphene} hooks
+    @staticmethod
+    def to_global_id(gtype, id):
+        """
+        Encode the {type} and {id} of a {node} implementor
+        """
+        # just splice them together
+        return f"{gtype}:{id}"
+
+
+    @staticmethod
+    def get_node_from_global_id(info, gid, only_type=None):
+        """
+        Look up and return a node instance based on its type and id as encoded in {gid}
+        """
+        # unpack
+        gtype, _id = gid.split(":")
+        # if the caller provided a type constraint that can't be satisfied
+        if only_type and gtype != only_type:
+            # get the journal
+            import journal
+            # treat this as a bug, for now
+            channel = journal.firewall("flocor.gql.node")
+            # and complain
+            channel.log(f"incompatible types: expected {only_type}, got {gtype}")
+
+        # map the typename to the known types
+        # leave blank for now, until we know under what conditions this gets invoked
+        # the firewall below will ring when that happens
+
+        # anything else is a bug
+        import journal
+        # make a channel
+        channel = journal.firewall("flocor.gql.node")
+        # and complain
+        channel.log(f"while resolving {gid}: unknown type {gtype}")
+
+        # UNREACHABLE
+        return
 
 
 # end of file
