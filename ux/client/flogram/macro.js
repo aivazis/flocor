bbe45e7f5f8e85872237f98db4723b7f858b243d
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-04-03 09:27:39 -0700

    ux/flogram:factored out the rendering of a single {macro} node to its own component

---
+++ ux/client/flogram/macro.js
@@ -0,0 +1,26 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// externals
+import React from 'react'
+// project
+import { Product } from '~/shapes'
+// local
+import { Node } from './node'
+
+
+// render the flow nodes that are macros
+export const Macro = ({ macro }) => {
+    // render
+    return (
+        <Node position={macro.position} >
+            <Product />
+        </Node>
+    )
+}
+
+
+// end of file
