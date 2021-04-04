05be4941bcf68c51470a461884123e12b3bdbbcc
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-04-04 13:48:04 -0700

    ux/flogram: the {context} now provides support for marking a node as a candidate for repositioning in the diagram

--- ux/client/flogram/context.js
+++ ux/client/flogram/context.js
@@ -14,15 +14,23 @@ const complaint = "while accessing the 'flogram' context: no provider"
 export const Context = React.createContext(
     // the default value that consumers see when accessing the context outside a provider
     {
+        // the moving node candidate
+        movingNode: null,
+        // a callback to mark a new node as the moving candidate
+        setMovingNode: () => { throw new Error(complaint)},
     }
 )
 
 
 // the provider factory
 export const Provider = ({ children }) => {
-    console.log("new context for 'flogram'")
+    // non-null indicates that a node was clicked and is a candidate for moving
+    const [movingNode, setMovingNode] = React.useState(null)
+
     // build the current value of the context
     const context = {
+        // moving node management
+        movingNode, setMovingNode,
     }
 
     // provide for my children
