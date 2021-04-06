79b5e948f6372be572af24ab834e61be5b3d29da
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-04-05 22:01:56 -0700

    ux/widgets: {tray}, the panel used to list the available nodes, is now a widget

---
+++ ux/client/widgets/tray/styles.js
@@ -0,0 +1,40 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// get colors
+import { theme } from '~/palette'
+
+
+// publish
+export default {
+    // the overall container
+    box: {
+    },
+
+    // the tray header
+    header: {
+        // style
+        fontSize: "60%",
+        padding: "0.5rem 0.0rem 0.5rem 0.75rem",
+        // colors
+        color: "hsl(0deg, 0%, 80%, 1)",
+        backgroundColor: "hsl(0deg, 0%, 31%, 1)",
+    },
+
+    // the tray title
+    title: {
+        // style
+        fontFamily: "rubik",
+        textTransform: "uppercase",
+    },
+
+    // the container of the children
+    items: {
+    },
+}
+
+
+// end of file
