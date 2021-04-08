a6841dec18e4216cb01bcafa5e8fc39cfa40f90b
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-04-08 09:11:44 -0700

    ux/trays: primed the {tray} that will contain the {calc} operators

---
+++ ux/client/trays/calc/index.js
@@ -0,0 +1,28 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// externals
+import React from 'react'
+import { graphql, useLazyLoadQuery } from 'react-relay/hooks'
+
+// project
+// i'm a try
+import { Tray } from '~/widgets'
+// styles
+import styles from './styles'
+
+
+// a tray with a flow node
+export const Calc = ({ style }) => {
+    // paint me
+    return (
+        <Tray title="calc operators" >
+        </Tray >
+    )
+}
+
+
+// end of file
---
+++ ux/client/trays/calc/styles.js
@@ -0,0 +1,34 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// get colors
+import { theme } from '~/palette'
+
+
+// publish
+export default {
+    // the overall container
+    box: {},
+
+    // the tray header
+    header: {},
+
+    // the tray title
+    title: {},
+
+    // the entries
+    items: {},
+
+    // the container of my icon
+    node: {},
+
+    // and its shape
+    shape: {},
+
+}
+
+
+// end of file
