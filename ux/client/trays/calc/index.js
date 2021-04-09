1a43de990f2b9a61175e028a125bb9dcc37ab46f
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-04-09 07:55:13 -0700

    ux/trays: added a sample {factory} to the {calc} tray

--- ux/client/trays/calc/index.js
+++ ux/client/trays/calc/index.js
@@ -9,17 +9,56 @@ import React from 'react'
 import { graphql, useLazyLoadQuery } from 'react-relay/hooks'
 
 // project
-// i'm a try
+// i'm a tray
 import { Tray } from '~/widgets'
+// of nodes
+import { Node } from '~/trays'
+// that are factories
+import { Factory } from '~/shapes'
 // styles
 import styles from './styles'
 
 
 // a tray with a flow node
 export const Calc = ({ style }) => {
+
+    // placeholders for the factory metadata
+    const factory = {
+        schema: "pyre.calc.plus",
+        category: "operators",
+    }
+
+    // build a graphical representation of my items:
+    // pick a size for my unit cell
+    const size = 24
+
+    // turn it into a box
+    const box = { x: 3 * size, y: 2 * size }
+    // build the transform to resize my shape; don't forget that the diagram shapes are rendered
+    // assuming a quarter cell grid, which means that they occupy the box ((-1,-1), (1,1)) in
+    // their intrinsic coordinates
+    const shrink = `scale(${size / 2}) translate(3 3)`
+    // mix my paint
+    const nodeStyle = { ...styles.node, ...style?.node }
+    const shapeStyle = { ...styles.shape, ...style?.shape }
+    // draw my shape
+    const shape = (
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
+            width={box.x} height={box.y} style={nodeStyle}>
+            <g transform={shrink}>
+                <Factory style={shapeStyle} />
+            </g>
+        </svg>
+    )
+
+
+
     // paint me
     return (
         <Tray title="calc operators" >
+            <Node key={factory.schema}
+                category={factory.category} family={factory.schema}
+                shape={shape} size={box} />
         </Tray >
     )
 }
