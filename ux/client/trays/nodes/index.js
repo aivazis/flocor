2155f3d8808af30dc5bf473c63ba2ebf4ddc127d
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-04-15 12:16:14 -0700

    ux/trays: added export of flow {nodes}

---
+++ ux/client/trays/nodes/index.js
@@ -0,0 +1,12 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// publish
+// the base node renderer
+export { Node } from './node'
+
+
+// end of file
