05dc6e3965b15acea49ebb34774a3893d2ff3642
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-04-15 13:41:31 -0700

    ux/trays: {factory} and its styling are now tray {items}

---
+++ ux/client/trays/items/factory/styles.js
@@ -0,0 +1,25 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// get colors
+import { theme } from '~/palette'
+
+
+// publish
+export default {
+    // the binding lines
+    binding: {
+        // stroke
+        stroke: "hsl(0deg, 0%, 35%)",
+        strokeWidth: 2,
+        vectorEffect: "non-scaling-stroke",
+        // fill
+        fill: "none",
+    },
+}
+
+
+// end of file
