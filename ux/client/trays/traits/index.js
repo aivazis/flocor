1105c35e38ce6ef356c552125346a3ab3a8ffa84
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-04-16 20:18:30 -0700

    ux/trays: the {traits} tray now uses the new {product} item renderer to populate itself

--- ux/client/trays/traits/index.js
+++ ux/client/trays/traits/index.js
@@ -8,51 +8,26 @@
 import React from 'react'
 import { graphql, useLazyLoadQuery } from 'react-relay/hooks'
 
-// locals
+// project
 // i am a tray
 import { Tray } from '~/widgets'
-// of nodes
-import { Item } from '~/trays'
-// that are products
-import { Product } from '~/shapes'
+// of nodes that are products
+import { Product } from '~/trays'
+// locals
 // styles
 import styles from './styles'
 
 
-// a tray with a flow node
+// a tray with product nodes
 export const Traits = ({ els, style }) => {
     // get the set of basic traits from the server
     const { traits } = useLazyLoadQuery(traitsQuery)
-
-    // build a graphical representation of my items
-    // we are in a quad cell grid
-    const box = { x: 2, y: 2 }
-    // build the transform to resize my shape; don't forget that the diagram shapes are rendered
-    // assuming a quarter cell grid, which means that they occupy the box ((-1,-1), (1,1)) in
-    // their intrinsic coordinates
-    const shrink = `scale(${els}) translate(1 1)`
-    // mix my paint
-    const nodeStyle = { ...styles.node, ...style?.node }
-    const shapeStyle = { ...styles.shape, ...style?.shape }
-
-    // draw my shape
-    const shape = (
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
-            width={els * box.x} height={els * box.y} style={nodeStyle}>
-            <g transform={shrink}>
-                <Product style={shapeStyle} />
-            </g>
-        </svg>
-    )
 
     // paint me
     return (
         <Tray title="calc variables" >
             {traits.map(trait => (
-                <Item key={trait.schema} category={trait.category} family={trait.schema}
-                    els={els} size={box}>
-                    {shape}
-                </Item>
+                <Product key={trait.schema} product={trait} els={els} style={style} />
             ))}
         </Tray >
     )
