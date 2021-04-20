8bcbe654d717163087ba90f2cde54e1815e98e9e
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-04-20 12:12:46 -1000

    flocor.flows: fixed the {schema} extraction and use in {variable}

--- pkg/flows/Variable.py
+++ pkg/flows/Variable.py
@@ -11,22 +11,25 @@ import flocor
 
 
 # a simple node
-class Macro:
+class Variable:
 
 
     # metamethods
-    def __init__(self, name=None, family="int", **kwds):
+    def __init__(self, name=None, family=None, **kwds):
         # chain up
         super().__init__(**kwds)
+
+        # extract the schema
+        schema = family.split('.')[-1]
 
         # make a uuid
         gid = uuid.uuid1()
         # build my id by folding in my type
-        self.pyre_id = f"Macro:{gid}"
+        self.pyre_id = f"Variable:{gid}"
         # attach my name
         self.pyre_name = name
         # look up my type
-        self.pyre_schema = getattr(flocor.schemata, family)
+        self.pyre_schema = getattr(flocor.schemata, schema)
 
         # all done
         return
