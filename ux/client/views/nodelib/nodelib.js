47386c3da7bea7e0407fcddd154bdc242d413787
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-04-19 14:59:22 -1000

    ux/views: {nodelib} now clears the new node info if {mouseup} triggers anywhere within its client area

--- ux/client/views/nodelib/nodelib.js
+++ ux/client/views/nodelib/nodelib.js
@@ -16,23 +16,30 @@ import { Operators, Variables } from '~/trays/calc'
 // locals
 // styles
 import styles from './styles'
+import { useClearNewNode } from '~/views/flo2d'
 
 
 // the trays
 export const NodeLibrary = ({ style }) => {
     // grab the app interaction element
     const appContainerRef = useInteractionContainer()
+    // and the callback that clears the new node info
+    const clearNewNode = useClearNewNode()
 
     // pick a length scale; this is the pixel size of a grid cell and it sets the dimensions
     // of tray items
     const els = 10
 
+    // set up my event handlers
+    const controls = {
+        onMouseUp: clearNewNode,
+    }
+
     // mix my paint
     const boxStyle = { ...styles.box, ...style?.box }
-
-    // paint me
+    // and paint me
     return (
-        <section style={boxStyle}>
+        <section style={boxStyle} {...controls}>
             <Shadow ref={appContainerRef} >
                 <Variables els={els} />
                 <Operators els={els} />
