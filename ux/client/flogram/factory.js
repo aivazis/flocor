fd713cbae4e4dce067b7d923220434ab797b0cf8
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-04-20 21:16:12 -1000

    ux/flogram: the {factory} implementation is now modeled after {product}

--- ux/client/flogram/factory.js
+++ ux/client/flogram/factory.js
@@ -6,74 +6,30 @@
 
 // externals
 import React from 'react'
+import { graphql, useFragment } from 'react-relay/hooks'
 // project
-// shapes
-import { Factory as FactoryShape, Plex, Slot, Terminal } from '~/shapes'
+import { Factory as Shape } from '~/shapes'
 // local
-// styling
-import styles from './styles'
-
-// render the shape
-export const Factory = ({ inputs = [], outputs = [], style }) => {
-    // make a narrow factory
-    const cell = 1
-    // get the number of inputs
-    const nInputs = inputs.length
-    // and the number of outputs
-    const nOutputs = outputs.length
-
-    // input multiplexing
-    const inplex = (
-        <g transform={`translate(${-cell} 0)`}>
-            {nInputs ? <Plex /> : <Terminal />}
-        </g>
-    )
-    // output multiplexing
-    const outplex = (
-        <g transform={`translate(${cell} 0)`}>
-            {nOutputs ? <Plex /> : <Terminal />}
-        </g>
-    )
+import { Node } from './node'
 
 
+// render the flow nodes that are factories
+export const Factory = (props) => {
+    const factory = useFragment(graphql`
+        fragment factory_factory on Factory {
+            id
+            position {
+                x
+                y
+            }
+        }
+    `, props.factory)
 
-    // paint me
+    // render
     return (
-        <>
-            {inputs.map((product, idx) => {
-                // the x coordinate of the slot
-                const x = -(cell + 2)
-                // and the y coordinate
-                const y = 2 * idx + 1 - nInputs
-                // render
-                return (
-                    <g key={product.label} >
-                        <path d={`M ${x} ${y} L ${x + 1} ${y} L -1 0`} style={styles.binding} />
-                        <g transform={`translate(${x} ${y})`}>
-                            <Slot />
-                        </g>
-                    </g>
-                )
-            })}
-            {outputs.map((product, idx) => {
-                // the x coordinate of the slot
-                const x = cell + 2
-                // and the y coordinate
-                const y = 2 * idx + 1 - nOutputs
-                // render
-                return (
-                    <g key={product.label} >
-                        <path d={`M ${x} ${y} L ${x - 1} ${y} L 1 0`} style={styles.binding} />
-                        <g transform={`translate(${x} ${y})`}>
-                            <Slot />
-                        </g>
-                    </g>
-                )
-            })}
-            <FactoryShape cell={cell} />
-            {inplex}
-            {outplex}
-        </>
+        <Node id={factory.id} position={factory.position} >
+            <Shape />
+        </Node>
     )
 }
 
