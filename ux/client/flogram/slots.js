d9dc6503aa43c434da4b1e7dc75a03a879a62d19
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-04-22 18:19:18 -1000

    ux/flogram: added a renderer for {slots}; empty for now

---
+++ ux/client/flogram/slots.js
@@ -0,0 +1,47 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// externals
+import React from 'react'
+import { graphql, useFragment } from 'react-relay/hooks'
+
+
+// render the flow nodes that are products
+export const Slots = ({ flow }) => {
+    // extract the list of products
+    const { slots } = useFragment(slotsFragment, flow)
+    console.log(slots)
+    // render
+    return null
+    // render
+    return (
+        <>
+            {
+                slots.edges.map(edge => (
+                    <Slot key={edge.node.id} slot={edge.node} />
+                ))
+            }
+        </>
+    )
+}
+
+
+//  the query fragment that fetches products
+const slotsFragment = graphql`
+    fragment slots_flow on Flow {
+        slots(first: 10000) @connection(key: "slotsFragment_slots") {
+            edges {
+                node {
+                    id
+                    # ...slot_slot
+                }
+                cursor
+            }
+        }
+    }
+`
+
+// end of file
