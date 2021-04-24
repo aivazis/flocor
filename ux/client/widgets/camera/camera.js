c259831b5e466a510f7e399f9fe38bffcb849a4b
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-04-24 09:30:13 -1000

    ux/widgets: cleaned up the {camera} implementation

--- ux/client/widgets/camera/camera.js
+++ ux/client/widgets/camera/camera.js
@@ -20,14 +20,22 @@ import styles from './styles'
 const Lens = ({ style, children }) => {
     // the camera factory
     const { camera, els } = useCamera()
+
+    // unpack the camera parameters
+    const { x, y, z, phi } = camera
+    // compute the corresponding viewpoort coordinates
+    const vx = els * x
+    const vy = els * y
+    const vz = els / z
+
     // pan
-    const panXform = `translate(${els * camera.x} ${els * camera.y})`
+    const pan = `translate(${vx} ${vy})`
     // zoom
-    const zoomXform = `scale(${els / camera.z})`
+    const zoom = `scale(${vz})`
     // orient
-    const twistXform = `rotate(${camera.phi})`
+    const twist = `rotate(${phi})`
     // assemble the transform
-    const pov = panXform + " " + zoomXform + " " + twistXform
+    const pov = [pan, zoom, twist].join(" ")
 
     // mix my paint
     const cameraStyle = { ...styles, ...style }
@@ -42,13 +50,13 @@ const Lens = ({ style, children }) => {
 
 
 // turn it into a context provider and publish it
-// {scale} is a length scale that is intrinsic to the contents; it converts a unit length in
-// content to a number of view port pixels
+// {scale} is a length scale that is intrinsic to the contents; it describes the size in viewport
+// pixels of the unit length in the content coordinate system
 export const Camera = React.forwardRef(({ scale = 25, style, children }, viewRef) => {
     // set up he context provider and install the lens
     return (
         <Provider ref={viewRef} scale={scale}>
-            <Lens >
+            <Lens style={style} >
                 {children}
             </Lens>
         </Provider>
