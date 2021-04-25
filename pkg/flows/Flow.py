0733de9eb1cad0f78a6c3564b38b5e852a9ffd9b
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-04-24 18:53:43 -1000

    flocor.flows: the default {flow} type is now a {dynamic} flow; it has its own containers for {factories} and {products}, so the entire interface is now obsolete

--- pkg/flows/Flow.py
+++ pkg/flows/Flow.py
@@ -11,59 +11,10 @@ import flocor
 
 
 # an undifferentiated flow
-class Flow(flocor.flow.workflow, family="flocor.flows.flow", implements=flocor.specs.flow):
+class Flow(flocor.flow.dynamic, family="flocor.flows.flow", implements=flocor.specs.flow):
     """
     An undifferentiated container for flow nodes
     """
-
-    # factoriies
-    def addFactory(self, node):
-        """
-        Add a new {node} to the set
-        """
-        # easy enough
-        self.factories[node.pyre_id] = node
-        # all done
-        return
-
-
-    def removeFactory(self, node):
-        """
-        Remove {node} from the environment
-        """
-        # discard from the node pile
-        del self.factories[node]
-        # and if it has a name
-        if node.pyre_name in self.factories:
-            # remove it from there as well
-            del self.factories[node.pyre_name]
-        # all done
-        return
-
-
-    # products
-    def addProduct(self, node):
-        """
-        Add a new {node} to the set
-        """
-        # easy enough
-        self.products[node.pyre_id] = node
-        # all done
-        return
-
-
-    def removeProduct(self, node):
-        """
-        Remove {node} from the environment
-        """
-        # discard from the node pile
-        del self.products[node]
-        # and if it has a name
-        if node.pyre_name in self.products:
-            # remove it from there as well
-            del self.products[node.pyre_name]
-        # all done
-        return
 
 
     # metamethods
@@ -72,11 +23,6 @@ class Flow(flocor.flow.workflow, family="flocor.flows.flow", implements=flocor.s
         super().__init__(**kwds)
         # set my uuid
         self.pyre_id = uuid.uuid1()
-
-        # initialize the sets of nodes
-        self.factories = {}
-        self.products = {}
-
         # all done
         return
 
