bbb5149554418dca8a0d54c6246051bdf5df3cf5
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-04 01:09:36 -0700

    flocor.diagram: added the {factory} rep

---
+++ pkg/diagram/Factory.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# framework
+import flocor
+
+# superclass
+from .Node import Node
+# factory builds these
+from .Slot import Slot
+
+
+# factories
+class Factory(Node):
+    """
+    The representation of a factory
+    """
+
+
+    # constants
+    typename = "Factory"
+
+
+    # interface
+    def connect(self, trait, slot):
+        """
+        Add {slot} as the representative of my {trait}
+        """
+        # add it to the pile
+        self.slots[trait] = slot
+        # and let it know
+        # N.B.: {slot} depends on {factory} for this update; don't modify without reading through
+        # the implementations of both {Factory} and {Slot}
+        slot.connect(factory=self, trait=trait)
+        # all done
+        return
+
+
+    # metamethods
+    def __init__(self, factory, **kwds):
+        # chain up
+        super().__init__(**kwds)
+        # save my product
+        self.factory = factory
+        # my connections
+        self.slots = {trait: slot for trait,slot in self._connections()}
+        # all done
+        return
+
+
+    # implementation details
+    def _connections(self):
+        """
+        Return all my input and output slots
+        """
+        # grab my factory
+        factory = self.factory
+        # make slots for my inputs
+        yield from self._makeSlots(traits=factory.pyre_inputTraits, inputs=True)
+        # and my outputs
+        yield from self._makeSlots(traits=factory.pyre_outputTraits, inputs=False)
+        # all done
+        return
+
+
+    def _makeSlots(self, traits, inputs=True):
+        """
+        Build slots corresponding to all {traits}
+        """
+        # get my location
+        x, y = self.position
+        # pick the side of the slots
+        side = 1 if inputs else -1
+        # realize the {traits}, just in case we were handed a virtual container
+        traits = tuple(traits)
+        # find out how many traits there are; we use this to position the slots in the diagram
+        nTraits = len(traits)
+        # go through all {traits}
+        for idx, trait in enumerate(traits):
+            # make a position for this slot
+            position = (x-3*side, y + 2*idx + 1 - nTraits)
+            # build an unbound rep
+            slot = Slot(product=None, position=position)
+            # connect me to it; don't forget that we are in the middle of building the
+            # connection table, so it gets updated automatically after we are done here
+            slot.connect(factory=self, trait=trait)
+            # and publish it
+            yield trait, slot
+        # all done
+        return
+
+
+# end of file
