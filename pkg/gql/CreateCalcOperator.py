5a5596af833e20448e1cc3da8528544fb8af56ea
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-04 19:07:42 -0700

    flocor.gql: the {createCalcOperator} mutation resolver now pulls its information from the {diagram}

--- pkg/gql/CreateCalcOperator.py
+++ pkg/gql/CreateCalcOperator.py
@@ -48,37 +48,28 @@ class CreateCalcOperator(graphene.Mutation):
 
         # get the panel
         panel = info.context["panel"]
-        # so we can grab the {flow}; we can use {owner} to do a consistency check that
-        # the node is being added to the correct graph
-        flow = panel.flow
-        # grab the {layout}
-        layout = panel.layout
-        # and the connectivity matrix
-        connectivity = panel.connectivity
+        # and its diagram
+        diagram = panel.diagram
 
         # make a {factory}; we don't have a name for it yet
         op = flocor.flows.operator(family=family)
-        # get the inputs
-        inputs = op.pyre_inputTraits
-        # find out how many there are
-        nInputs = len(inputs)
-        # now, the outputs
-        outputs = op.pyre_outputTraits
-        # find out how many there are
-        nOutputs = len(outputs)
+        # add it to the diagram and get its rep
+        factory = diagram.addFactory(factory=op, position=(x,y))
 
-        # add it to the flow
-        flow.index[op.pyre_id] = op
-        flow.factories.add(op)
-        # and the layout
-        layout[op.pyre_id] = {"x": x, "y": y}
-
-        # build the representation of the factory
-        # make a position
+        # get the guid
+        guid = factory.guid
+        # name
+        name = factory.factory.pyre_name
+        # family
+        family = factory.factory.pyre_family()
+        # arity
+        inputs = factory.inputs
+        outputs = factory.outputs
+        # make a rep for the factory position
         position = Position(x=x, y=y)
-        # make a factory node
-        node = Factory(id=f"Factory:{op.pyre_id}", family=family,
-                       inputs=nInputs, outputs=nOutputs,
+        # and for the factory itself
+        node = Factory(id=guid, name=name, family=family,
+                       inputs=inputs, outputs=outputs,
                        position=position)
 
         # assemble the slots and their connectors
@@ -87,79 +78,35 @@ class CreateCalcOperator(graphene.Mutation):
         # and one for connectors
         connectors = []
 
-        # for each input
-        for idx, trait in enumerate(inputs):
-            # get the slot
-            slot = op.pyre_inventory[trait]
-            # get its id
-            guid = slot.pyre_id
-            # add it to the index
-            flow.index[guid] = slot
-            # if it's bound
-            if slot._value is not None:
-                # skip it
-                continue
-            # get its name
-            name = trait.name
-            # its family
-            family = trait.typename
-            # compute its coordinate
-            sx = x -3
-            sy = y + 2*idx + 1 - nInputs
-            # add the node to the diagram layout
-            layout[guid] = {"x": sx, "y": sy}
-            # update the connectivity matrix
-            connectivity[op.pyre_id][guid] = True
-            connectivity[guid][op.pyre_id] = True
-            # build its position rep
-            slotPosition = Position(x=sx, y=sy)
-            # build the rep
-            slotRep = Slot(id=f"Slot:{guid}", name=name, family=family, position=slotPosition)
-            # and add it to the pile
-            slots.append(slotRep)
-            # build the connector uid
-            bid = f"Connector:{op.pyre_id}|{guid}"
-            # build the connector rep
-            connectorRep = Connector(id=bid, inp=True, factoryAt=position, productAt=slotPosition)
-            # and add to its pile
-            connectors.append(connectorRep)
-
-        # for each output
-        for idx, trait in enumerate(outputs):
-            # get the slot
-            slot = op.pyre_inventory[trait]
-            # get its id
-            guid = slot.pyre_id
-            # add it to the index
-            flow.index[guid] = slot
-            # if it's bound
-            if slot._value is not None:
-                # skip it
-                continue
-            # get its name
-            name = trait.name
-            # its family
-            family = trait.typename
-            # form its coordinates
-            sx = x + 3
-            sy = y + 2*idx + 1 - nOutputs
-            # add the slot to the diagram layout
-            layout[guid] = {"x": sx, "y": sy}
-            # update the connectivity matrix
-            connectivity[op.pyre_id][guid] = False
-            connectivity[guid][op.pyre_id] = False
-            # build its position rep
-            slotPosition = Position(x=sx, y=sy)
-            # build the rep
-            rep = Slot(id=f"Slot:{guid}", name=name, family=family, position=slotPosition)
+        # go through the slots, and for each one
+        for slot in factory.slots.values():
+            # grab its id
+            guid = slot.guid
+            # look up its position
+            x,y = slot.position
+            # represent
+            slotAt = Position(x=x, y=y)
+            rep = Slot(id=guid, position=slotAt)
             # and add it to the pile
             slots.append(rep)
-            # build the connector uid
-            bid = f"Connector:{op.pyre_id}|{guid}"
-            # build the connector rep
-            connectorRep = Connector(id=bid, inp=False, factoryAt=position, productAt=slotPosition)
-            # and add to its pile
-            connectors.append(connectorRep)
+
+            # now, each slot has a pile of connections; this is a new factory, so it is
+            # certain that each slot has only one connection to the factory we just added;
+            # what we don't know is whether this is an input or an output slot; so do it right...
+            for client, trait in slot.connectors:
+                # make a connector id
+                cuid = f"Connector:{client.pyre_id}|{slot.pyre_id}"
+                print(cuid)
+                # unpack the factory position
+                fx, fy = client.position
+                # make a rep for the factory position
+                factoryAt = Position(x=fx, y=fy)
+                # deduce the direction
+                direction = trait.input
+                # build the rep
+                rep = Connector(id=cuid, inp=direction, factoryAt=factoryAt, productAt=slotAt)
+                # and add it to the pile
+                connectors.append(rep)
 
         # build the payload and return it
         return CreateCalcOperator(flow=owner, node=node, slots=slots, connectors=connectors)
