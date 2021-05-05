b12f713727862cbe8ff6287b74804784ba43bce1
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-04 19:10:21 -0700

    flocor.gql: first pass at implementing the {moveNodeEnd} mutation resolver using information from the {diagram}

--- pkg/gql/MoveNodeEnd.py
+++ pkg/gql/MoveNodeEnd.py
@@ -57,135 +57,55 @@ class MoveNodeEnd(graphene.Mutation):
 
         # get the panel
         panel = info.context["panel"]
-        # the flow
-        flow = panel.flow
-        # its {layout}
-        layout = panel.layout
-        # and the connectivity matrix
-        connectivity = panel.connectivity
-
+        # and the diagram
+        diagram = panel.diagram
         # extract the type of the node and its {pyre_id}
-        typename, nodeid = id.split(":")
+        _, nodeid = id.split(":")
         # convert the string into a {uuid}
         guid = uuid.UUID(hex=nodeid)
+        # look up the moving node
+        node = diagram.nodes[guid]
+        # pack the new position of the node
+        here = (x, y)
 
-        # collision check:
-        # N.B.: all illegal collisions are rejected while the node is moving; our only
-        #       job here is to detect collisions that result in topological changes to the
-        #       diagram and handle them
-        # go through all the known nodes
-        for nid, loc in layout.items():
-            # look for one that occupies my cell
-            if nid != guid and loc["x"] == x and loc["y"] == y:
-                # got it; stop looking
-                break
-        # if the search yielded no collisions
-        else:
+        # do the move
+        target, connections = diagram.move(node=node, position=here)
+
+        # if there was no collision
+        if target is None:
             # nothing further to do
             return MoveNodeEnd(flow=owner)
 
-        # look up the moving node
-        moving = flow.index[guid]
-        # and the stationary target
-        target = flow.index[nid]
+        # build a rep for the current position
+        here = Position(x=x, y=y)
+        # the {target} is always the node that gets discarded
+        deadnode = target.guid
 
-        # if the merge is disallowed
-        if False:
-            # get the moving node's last known good location
-            x, y = panel.lastpos
-            # use it to update the layout
-            layout[guid] = {"x" : x, "y" : y}
-            # build a rep for it
-            position = Position(x=x, y=y)
-            # if the moving node is a product
-            if isinstance(moving, flocor.flows.product):
-                # build a product rep
-                node = Product(id=id, position=position)
-            # if it's a slot
-            elif isinstance(moving, flocor.flows.slot):
-                # build a slot rep
-                node = Slot(id=id, position=position)
-            # otherwise
-            else:
-                # get the journal
-                import journal
-                # make a channel
-                channel = journal.firewall("flocor.gql.schema")
-                # and complain
-                channel.log(f"while moving node '{id}': unknown type '{typename}")
-                # and, just in case firewalls are not fatal, send an empty result back
-                return MoveNodeEnd(flow=owner)
-            # adjust the connectors
-            connectors = []
-            # by going through all the factories the moving node is connected to
-            for fuid, direction in connectivity[guid].items():
-                # look up the factory position
-                fpos = layout[fuid]
-                # build a rep for it
-                frep = Position(x=fpos["x"], y=fpos["y"])
-                # assemble the connector id
-                buid = f"Connector:{fuid}|{guid}"
-                # build a rep for the connector
-                brep = Connector(id=buid, inp=direction, factoryAt=fpos, productAt=position)
-                # and add it to the pile
-                connectors.append(brep)
-            # build the payload and return it
-            return MoveNodeEnd(flow=owner, node=node, connectors=connectors)
-
-        # time to bind...
-        # binding means:
-        # - one of the two nodes is removed from the diagram
-        # - all of its connectors have to be removed as well
-        # - the surviror gets the all the connectors of the dead node
-        #  pick the surviror
-
-        # the current position
-        position = Position(x=x, y=y)
-
-        # if the target is a product
-        if isinstance(target, flocor.flow.product):
-            # it is the survivor
-            survivor = target
-            # and the moving node dies
-            dead = moving
-        # otherwise
-        else:
-            # the reverse
-            survivor = moving
-            dead = target
-
-        # the dead guy is always a slot, for now
-        deadnode = f"Slot:{dead.pyre_id}"
         # make a pile for the new connectors of the survivor
         new = []
         # and a pile for the discarded connectors of the dead node
         discard = []
-        # go through all the factories the dead node is connected to
-        for fuid, direction in connectivity[dead.pyre_id].items():
+
+        # go through the {target} connections
+        for factory, trait, _ in connections:
             # look up the factory position
-            fpos = layout[fuid]
+            fx, fy = factory.position
             # build a rep for it
-            frep = Position(x=fpos["x"], y=fpos["y"])
-            # make a connector id for the new connector
-            nuid = f"Connector:{fuid}|{survivor.pyre_id}"
-            # build a rep for the connector
-            nrep = Connector(id=nuid, inp=direction, factoryAt=fpos, productAt=position)
+            fpos = Position(x=fx, y=fy)
+            # make an id for the new connector
+            nuid = f"Connector:{factory.pyre_id}|{target.pyre_id}"
+            # so we can build a rep for it
+            nrep = Connector(id=nuid, inp=trait.input, factoryAt=fpos, productAt=here)
             # and add it to the pile
             new.append(nrep)
-            # update the connectivity matrix
-            connectivity[survivor.pyre_id][fuid] = direction
-            connectivity[fuid][survivor.pyre_id] = direction
-            # make a connector id for the discarded connector
-            duid = f"Connector:{fuid}|{dead.pyre_id}"
+            # now, make an id for the discarded connector
+            duid = f"Connector:{factory.pyre_id}|{target.pyre_id}"
             # and add it to the pile
             discard.append(duid)
-            # update the connectivity matrix
-            del connectivity[fuid][dead.pyre_id]
-        # remove the remaining connections to the dead node
-        del connectivity[dead.pyre_id]
-        # remove the slot from the layout
-        # del layout[dead.pyre_id]
 
+        print(f"moving {node} to {here}")
+        print(f"  new: {new}")
+        print(f"  discard: {discard}")
         # all done
         return MoveNodeEnd(flow=owner, dead=deadnode, new=new, discard=discard)
 
