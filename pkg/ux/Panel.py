2222d81f93b90be23e122d8b988e643ecefde405
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-05 17:18:30 -0700

    flocor.ux: the {panel} now delegates all graph management to {diagram}

--- pkg/ux/Panel.py
+++ pkg/ux/Panel.py
@@ -26,14 +26,8 @@ class Panel(flocor.shells.command, family="flocor.cli.ux"):
     def __init__(self, **kwds):
         # chain up
         super().__init__(**kwds)
-        # wrap a diagram on my {flow}
+        # wrap a diagram over my {flow}
         self.diagram = flocor.diagram.diagram(flow=self.flow)
-        # initialize the graph layout
-        self.layout = {}
-        # and the connectors
-        self.connectivity = flocor.patterns.vivify(levels=2, atom=bool)
-        # we also provide a place to park the last known good position of whatever node is moving
-        self.lastpos = None
         # all done
         return
 
