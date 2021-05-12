73e4c3b266bb9a50a912570b91e13795cae36cd6
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-12 09:22:09 -0700

    flocor.diagram: the {labels} of connectors that represent multiple {factory} traits being bound to the same {slot} now contain all {trait} names

--- pkg/diagram/Node.py
+++ pkg/diagram/Node.py
@@ -5,6 +5,7 @@
 
 
 # externals
+import collections
 import uuid
 
 
@@ -39,20 +40,32 @@ class Node:
         """
         Generate a label for this node
         """
-        # go through my connectivity table
+        # the connector between a given slot/factory pair may represent the binding of multiple
+        # traits to the same slot, e.g. when both operands of {add} are connected to the same
+        # input. we want the connector label to reflect the names of all traits that are mapped
+        # to their slot
+        uniq = collections.defaultdict(list)
+        # go through the my connectivity table
         for factory, trait, slot in self.connections:
+            # and add this relation to the table; we form the key using not only the
+            # {factory}/{slot} pair, but the trait category as well so that we can distinguish
+            # inputs and outputs, just in case we ever allow complicate diagram topologies
+            uniq[(factory,slot,trait.input)].append(trait)
+
+        # now, go through the table
+        for (factory, slot, category), traits in uniq.items():
             # get the factory position
             _, fy = factory.position
             # and the slot position
             sx, sy = slot.position
             # build the label position
-            lx = sx + (1 if trait.input else -1)
+            lx = sx + (1 if category else -1)
             ly = sy + (0.5 if sy > fy else -0.25)
             # pack and make available
             yield {
                "id": f"Connector:{factory.pyre_id}|{slot.pyre_id}-label",
-               "value": trait.name,
-               "category": "input" if trait.input else "output",
+               "value": ",".join(sorted(trait.name for trait in traits)),
+               "category": "input" if category else "output",
                "position": (lx, ly)
             }
         # all done
