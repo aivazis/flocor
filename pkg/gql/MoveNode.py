457cf9461ff49950ec317bbd1fa7a880fca77938
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-17 15:24:27 -0700

    flocor.gql: the {moveNode} mutation resolver is now significantly simpler and relies on {diagram} interface for assembling the deltas to ship to the client

--- pkg/gql/MoveNode.py
+++ pkg/gql/MoveNode.py
@@ -58,73 +58,33 @@ class MoveNode(graphene.Mutation):
         panel = info.context["panel"]
         # and the diagram
         diagram = panel.diagram
-        # extract the type of the node and its {pyre_id}
-        typename, nodeid = id.split(":")
-        # convert the string into a {uuid}
-        guid = uuid.UUID(hex=nodeid)
         # look up the moving node
-        node = diagram.nodes[guid]
+        node = diagram.findNode(relay=id)
+        # pack the new position
+        position = x, y
 
-        # if the move is illegal
-        if not diagram.mayMove(node=node, position=(x,y)):
+        # if the node is already at this position
+        if node.position == position:
+            # nothing to do
+            return MoveNode(flow=owner)
+
+        # attempt to move the node; if the move is disallowed
+        if not diagram.move(node=node, position=(x,y)):
             # bail without touching anything
             return MoveNode(flow=owner)
 
-        # build the new position rep to return to the client
-        here = Position(x=x, y=y)
-        # deduce the correct return type; for factories
-        if typename == "Factory":
-            # build a factory
-            rep = Factory(id=id, position=here)
-        # for slots
-        elif typename == "Slot":
-            # build a slot
-            rep = Slot(id=id, position=here)
-        # anything else
-        else:
-            # is a problem
-            import journal
-            # that's almost certainly a bug
-            channel = journal.firewall("flocor.gql.schema")
-            # and complain
-            channel.log(f"while moving node '{id}': unknown type '{typename}")
-            # and, just in case firewalls are not fatal, send an empty result back
-            return MoveNodeEnd(flow=owner)
+        # otherwise, make a pile of the node labels that require relocation
+        labels = list(node.labels)
 
-        # make a pile of labels
-        labels = []
-        # go through the node labels
-        for label in node.labels:
-            # build a rep for its position
-            label["position"] = Position(*label["position"])
-            # and one for the label
-            labelRep = Label(**label)
-            # and add to the pile
-            labels.append(labelRep)
-
-        # make a pile of connectors
-        connectors = []
-        # go through the connections of the node
-        for factory, trait, slot in node.connections:
-            # deduce the direction of the connector
-            direction = trait.input
-            # unpack the factory position
-            fx, fy = factory.position
-            # and build a rep
-            fpos = Position(x=fx, y=fy)
-            # unpack the slot position
-            sx, sy = slot.position
-            # and build a rep
-            spos = Position(x=sx, y=sy)
-            # assemble the connector id
-            buid = f"Connector:{factory.pyre_id}|{slot.pyre_id}"
-            # build a rep for the connector
-            brep = Connector(id=buid, inp=direction, factoryAt=fpos, slotAt=spos)
-            # and add it to the pile
-            connectors.append(brep)
+        # make a pile of the affected connectors
+        connectors = list(node.connections())
+        # go through them
+        for connector in connectors:
+            # and add their labels to the update pile
+            labels.extend(connector.labels)
 
         # return the node info
-        return MoveNode(flow=owner, node=rep, labels=labels, connectors=connectors)
+        return MoveNode(flow=owner, node=node, labels=labels, connectors=connectors)
 
 
 # end of file
