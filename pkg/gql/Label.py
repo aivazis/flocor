b8d8c593334c5dcbf8d27cb4d73ee7f1ab4fb694
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-17 15:19:34 -0700

    flocor.gql: {label} got resolvers backed by the new {flocor.diagram.label} instances

--- pkg/gql/Label.py
+++ pkg/gql/Label.py
@@ -14,7 +14,7 @@ from .Position import Position
 # a flow node that represents a typed+named variable
 class Label(graphene.ObjectType):
     """
-    A connector from a product to a factory
+    Representation of text at a specific location in the diagram
     """
 
     # the fields
@@ -22,6 +22,23 @@ class Label(graphene.ObjectType):
     value = graphene.List(graphene.String, required=False)
     category = graphene.String(required=False)
     position = graphene.Field(Position, required=True)
+
+
+    # the resolvers
+    def resolve_id(label, *_):
+        return label.relay
+
+    def resolve_value(label, *_):
+        return label.text
+
+    def resolve_category(label, *_):
+        return label.category
+
+    def resolve_position(label, *_):
+        # unpack the position
+        x, y = label.position
+        # build a position rep and return it
+        return Position(x=x, y=y)
 
 
 # end of file
