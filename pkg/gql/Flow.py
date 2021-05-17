4b3289e1b8a67c21c83854e23d39092cd39c17e8
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-17 15:22:43 -0700

    flocor.gql: the {flow} query resolver is now trivial and contains only uncpacking logic: all the data backed field resolution is now happening in the resolvers of individual fields; the only exception is the harvesting of all the labels from the {diagram}, which should also be abstracted away at some point

--- pkg/gql/Flow.py
+++ pkg/gql/Flow.py
@@ -52,6 +52,9 @@ class Flow(graphene.ObjectType):
     def resolve_id(panel, info, **kwds):
         # unpack
         flow = panel.flow
+        # debug
+        diagram = panel.diagram
+        print("\n".join(diagram.dump()))
         # return the {flow} id
         return f"Flow:{flow.pyre_id}"
 
@@ -73,123 +76,46 @@ class Flow(graphene.ObjectType):
     def resolve_factories(panel, info, **kwds):
         # unpack
         diagram = panel.diagram
-
-        # make a pile
-        factories = []
-        # go through the factories in the {diagram}
-        for node in diagram.factories:
-            # get its id
-            guid = node.guid
-            # its name
-            name = node.factory.pyre_name
-            # its family
-            family = node.factory.pyre_family()
-            # its arity
-            inputs = node.inputs
-            outputs = node.outputs
-            # look up its position
-            x,y = node.position
-            # represent
-            factory = Factory(id=guid,
-                        name=name, family=family,
-                        inputs=inputs, outputs=outputs,
-                        position=Position(x=x, y=y))
-            # and add to the pile
-            factories.append(factory)
-
         # return the pile
-        return factories
+        return list(diagram.factories)
 
 
     def resolve_slots(panel, info, **kwds):
         # unpack
         diagram = panel.diagram
-
-        # make a pile
-        slots = []
-        # slots are the unbound traits of factories
-        for slot in diagram.slots:
-            # grab its id
-            guid = slot.guid
-            # check whether it is bound
-            bound = False if slot.product is None else True
-            # look up its position
-            x,y = slot.position
-            # represent
-            rep = Slot(id=guid, bound=bound, position=Position(x=x, y=y))
-            # and add it to the pile
-            slots.append(rep)
-
-        # return the pile
-        return slots
+        # return the pile of slots
+        return list(diagram.slots)
 
 
     def resolve_connectors(panel, info, **kwds):
         # unpack
         diagram = panel.diagram
-
-        # make a pile
-        connectors = []
-        # all connectors involve slots so go through them
-        for node in diagram.slots:
-            # unpack the node position
-            nx, ny = node.position
-            # make a rep for the node position
-            nodeAt = Position(x=nx, y=ny)
-            # each node has a pile of connections
-            for factory, trait in node.connectors:
-                # make a connector id
-                guid = f"Connector:{factory.pyre_id}|{node.pyre_id}"
-                # unpack the factory position
-                fx, fy = factory.position
-                # make a rep for the factory position
-                factoryAt = Position(x=fx, y=fy)
-                # deduce the direction
-                direction = trait.input
-                # build the rep
-                rep = Connector(id=guid, inp=direction, factoryAt=factoryAt, slotAt=nodeAt)
-                # and add it to the pile
-                connectors.append(rep)
-
-        # return the pile
-        return connectors
+        # return the pile of connectors
+        return list(diagram.connectors)
 
 
     def resolve_labels(panel, info, **kwds):
         # unpack
         diagram = panel.diagram
 
-        # make a pile
-        labels = []
-        # go through the slots in the {diagram}
-        for node in diagram.slots:
-            # for each label
-            for label in node.labels:
-                # build a rep for its position
-                label["position"] = Position(*label["position"])
-                # and one for the label
-                rep = Label(**label)
-                # and add to the pile
-                labels.append(rep)
+        # initialize a pile with all the diagram labels
+        labels = list(diagram.labels)
 
-        # now to extract the factory labels
-        # N.B.: in order to avoid double counting, we are only interested in the first
-        # entry of the factory label set, which is guaranteed to be the name of the factory.
-        # the rest would be labels for its connections that we have already harvested by
-        # traversing the all the slots in the diagram, so we'll skip them
-        for node in diagram.factories:
-            # for each label
-            for label in node.labels:
-                # build a rep for its position
-                label["position"] = Position(*label["position"])
-                # and one for the label
-                rep = Label(**label)
-                # add it to the pile
-                labels.append(rep)
-                # and bail; the rest are already on the pile
-                break
+        # go through all the factories
+        for factory in diagram.factories:
+            # and add their labels to the pile
+            labels.extend(factory.labels)
 
-        # return the pile
+        # go through all the slots
+        for slot in diagram.slots:
+            # add their labels to the pile
+            labels.extend(slot.labels)
+            # go through their connections
+            for connector in slot.connections():
+                # and add the connector labels to the pile
+                labels.extend(connector.labels)
+
+        # return the pile of labels
         return labels
 
 
