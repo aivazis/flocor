6a504b1843fad9b256ce373bd04d5a9b41d7fa49
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-17 15:10:35 -0700

    flocor.diagram: connectors now come in two flavors: {input} and {output}; they match the markup of the {traits} they carry and know how to offset themselves in the diagram to accommodate the rendering strategy

---
+++ pkg/diagram/Output.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# superclass
+from .Connector import Connector
+
+
+# an input connector
+class Output(Connector):
+    """
+    Representation of a connector from a {factory} to one or more of its inputs
+    """
+
+    # constants
+    typename = "Output"
+
+
+    # interface
+    def add(self, trait):
+        """
+        Add {trait} to this connection
+        """
+        # if {trait} is indeed an output
+        if trait.output:
+            # add it to my pile
+            return super().add(trait=trait)
+
+        # otherwise, we have a problem
+        import journal
+        # which is almost certainly a bug
+        channel = journal.firewall("flocor.diagram.input")
+        # so complain
+        channel.line("while adding '{trait.name}' to {self}:")
+        channel.log("'{trait.name}' is not an output")
+        # just in case firewalls aren't fatal
+        return
+
+
+    # implementation details
+    def placeLabel(self):
+        """
+        Compute the location of my basic label
+        """
+        # unpack the factory position
+        _, fy = self.factory.position
+        # and the slot position
+        sx, sy = self.slot.position
+        # build the label position
+        lx = sx - 1
+        ly = sy + (0.5 if sy > fy else -0.25)
+        # pack it and ship it
+        return lx, ly
+
+
+# end of file
