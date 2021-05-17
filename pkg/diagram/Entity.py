99af31030f1325f821aaa33f479c8d30f74c205f
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-17 15:02:26 -0700

    flocor.diagram: added {entity} new base class for all entities that are representable on the diagram canvas

---
+++ pkg/diagram/Entity.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# externals
+import uuid
+
+
+# the base entity
+class Entity:
+    """
+    The base class for all diagram entities
+    """
+
+    # constants
+    typename = "Entity"
+
+
+    # public data
+    @property
+    def relay(self):
+        # build a {relay} node id
+        return f"{self.typename}:{self.eid}"
+
+
+    # interface
+    @staticmethod
+    def relayToEid(relay):
+        """
+        Extract my {typename} and {eid} from my {relay} id
+        """
+        # parse
+        typename, hex = relay.split(":")
+        # convert the {hex} key into a {uuid}
+        eid = uuid.UUID(hex=hex)
+        # pack and ship
+        return typename, eid
+
+
+    # metamethods
+    def __init__(self, name=None, **kwds):
+        # chain up
+        super().__init__(**kwds)
+        # all nodes have their own ids
+        self.eid = uuid.uuid1()
+        # record my name
+        self.name = name
+        # all done
+        return
+
+
+    def __str__(self):
+        # build my name
+        name = f" {self.name}" if self.name is not None else ""
+        # and my tag
+        tag = str(self.eid)[:8]
+        # render my {typename} and my {uuid}
+        return f"{self.typename}{name} '{tag}'"
+
+
+# end of file
