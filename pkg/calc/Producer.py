3acd8007fd31604ce409a07400529e85311c57b1
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-26 11:24:58 +0300

    flocor.calc: added a local {producer} protocol to establish the namespace

---
+++ pkg/calc/Producer.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# framework
+import flocor
+
+
+# protocol for local factories
+class Producer(flocor.flow.producer, family="flocor.calc.factories"):
+    """
+    The protocol for {flocor.calc} factories
+    """
+
+
+# end of file
