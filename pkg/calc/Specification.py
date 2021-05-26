d0094fbb82afe4ced5721e31ea5fb2f1f905df8d
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-26 11:26:10 +0300

    flocor.calc: added a local {specification} protocol to establish the namespace

---
+++ pkg/calc/Specification.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# framework
+import flocor
+
+
+# protocol for local products
+class Specification(flocor.flow.specification, family="flocor.calc.products"):
+    """
+    The protocol for {flocor.calc} products
+    """
+
+
+# end of file
