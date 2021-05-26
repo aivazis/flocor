0129317c72abe956710946a9a0b751e499903453
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-26 09:20:43 +0300

    flocor.flows: broke out the package {operators} into individual modules

---
+++ pkg/flows/Sub.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# framework
+import flocor
+# the base factory
+from .Factory import Factory
+
+
+# subtract {op2} from {op1}
+class Sub(Factory, family="flocor.calc.factories.sub"):
+    """
+    A factory that subtracts its two operands
+    """
+
+
+    # my operands
+    op1 = flocor.flow.specification.input()
+    op2 = flocor.flow.specification.input()
+    # my value
+    value = flocor.flow.specification.output()
+
+
+    # interface
+    def pyre_run(self, **kwds):
+        """
+        Compute and store my value
+        """
+        self.value.value = self.op1.value - self.op2.value
+        # all done
+        return self
+
+
+# end of file
