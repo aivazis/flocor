1adaa6621551a8bc5bb4cebaf5fc7c4c24a259da
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-29 10:21:18 +0300

    flocor.isce: assembled the {factories} namespace

---
+++ pkg/isce/factories/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# the framework
+import flocor
+# pull the {isce} protocols
+from .. import protocols
+
+
+# publish the base factory so users can extend the package
+from .Factory import Factory as factory
+
+
+@flocor.foundry(implements=protocols.slcFactory, tip="a reader for SLC data products")
+def slc():
+    """
+    An SLC reader
+    """
+    # pull the spec
+    from .SLCReader import SLCReader
+    # and publish it
+    return SLCReader
+
+
+# end of file
