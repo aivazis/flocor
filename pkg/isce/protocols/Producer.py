f3ec880ab56d05984bc81308e90220501cf7f93a
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-29 08:15:22 +0300

    flocor.isce: started moving the {isce} specific protocols to their now home

---
+++ pkg/isce/protocols/Producer.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# framework
+import flocor
+
+
+# protocol for local factories
+class Producer(flocor.flow.producer, family="flocor.isce.factories"):
+    """
+    The protocol for {flocor.isce} factories
+    """
+
+
+# end of file
---
+++ pkg/isce/protocols/Specification.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# framework
+import flocor
+
+
+# protocol for local products
+class Specification(flocor.flow.specification, family="flocor.isce.products"):
+    """
+    The protocol for {flocor.isce} products
+    """
+
+
+# end of file
