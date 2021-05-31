6f7571c46a99d6457b56a0667875438767d01d7d
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-31 10:21:13 +0300

    flocor.ampcor: added a stub for {planner}, the default implementation of the {correlationPlan} generator

---
+++ pkg/ampcor/factories/Planner.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# framework
+import flocor
+# superclass
+from .Factory import Factory as factory
+
+
+# protocol for describing maps
+class Planner(factory,
+              family="flocor.ampcor.factories.planners.planner",
+              implements=flocor.ampcor.protocols.planner):
+    """
+    Create a correlation plan given the reference and secondary raster shapes and a map
+    from pixels in the reference raster to pixels in the secondary raster
+    """
+
+
+    # inputs
+    refMeta = flocor.isce.protocols.slcMetadata.input()
+    refMeta.doc = "the metadata of the reference SLC product"
+
+    secMeta = flocor.isce.protocols.slcMetadata.input()
+    secMeta.doc = "the metadata of the secondary SLC product"
+
+    domain = flocor.isce.protocols.pixels.input()
+    domain.doc = "the input pixels"
+
+    range = flocor.isce.protocols.pixels.input()
+    range.doc = "the sequence of output pixels"
+
+    # output
+    plan = flocor.ampcor.protocols.correlationPlan.output()
+    plan.doc = "the correlation plan"
+
+
+# end of file
