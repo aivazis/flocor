c2033ac15a9323e2b2b48106cf8db491f4bd75f0
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-05-31 10:21:35 +0300

    flocor.ampcor: published the new {factories}

--- pkg/ampcor/factories/__init__.py
+++ pkg/ampcor/factories/__init__.py
@@ -14,4 +14,38 @@ from .. import protocols
 from .Factory import Factory as factory
 
 
+# foundries
+@flocor.foundry(implements=protocols.functor, tip="a uniform grid covering")
+def constant():
+    """
+    A map that applies a constant shift to its input pixels
+    """
+    # pull the spec
+    from .Constant import Constant
+    # and publish it
+    return Constant
+
+
+@flocor.foundry(implements=protocols.planner, tip="a generator of a correlation plan")
+def planner():
+    """
+    A generator of a correlation plan
+    """
+    # pull the spec
+    from .Planner import Planner
+    # and publish it
+    return Planner
+
+
+@flocor.foundry(implements=protocols.cover, tip="a uniform grid covering")
+def uniformGrid():
+    """
+    A covering that generates a uniform grid
+    """
+    # pull the spec
+    from .UniformGrid import UniformGrid
+    # and publish it
+    return UniformGrid
+
+
 # end of file
