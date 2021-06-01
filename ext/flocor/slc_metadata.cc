c50cfafcfe88aca57704508118ea1d4f32c741af
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-06-01 11:29:22 +0300

    flocor::products: cosmetics in the {slc_metadata} bindings

--- ext/flocor/slc_metadata.cc
+++ ext/flocor/slc_metadata.cc
@@ -15,9 +15,9 @@
 // type aliases
 namespace flocor::py {
     // the product spec
-    using slc_t = flocor::products::slc_metadata_t;
+    using slc_meta_t = flocor::products::slc_metadata_t;
     // safe pointers
-    using slc_pointer = std::unique_ptr<slc_t>;
+    using slc_pointer = std::unique_ptr<slc_meta_t>;
 
     // slc layout
     using layout2d_t = products::layout_t<2>;
@@ -33,7 +33,7 @@ void
 flocor::py::slc_metadata(py::module & m)
 {
     // add the product spec to the package namespace
-    auto slc = py::class_<slc_t>(m, "SLCMetadata");
+    auto slc = py::class_<slc_meta_t>(m, "SLCMetadata");
 
     // constructor from a python tuple
     slc.def(
@@ -41,11 +41,11 @@ flocor::py::slc_metadata(py::module & m)
             // unpack
             auto [lines, samples] = pyShape;
             // make a shape
-            slc_t::layout_type::shape_type shape { lines, samples };
+            slc_meta_t::layout_type::shape_type shape { lines, samples };
             // use it to make a layout
-            slc_t::layout_type layout { shape };
-            // and from there an {slc_t} spec
-            auto slc = new slc_t(layout);
+            slc_meta_t::layout_type layout { shape };
+            // and from there an {slc_meta_t} spec
+            auto slc = new slc_meta_t(layout);
             // dress it and return it
             return slc_pointer(slc);
         }),
@@ -56,9 +56,9 @@ flocor::py::slc_metadata(py::module & m)
     slc.def(
         py::init([](const shape2d_t & shape) {
             // make a layout
-            slc_t::layout_type layout { shape };
-            // and from that an {slc_t} spec
-            auto slc = new slc_t(layout);
+            slc_meta_t::layout_type layout { shape };
+            // and from that an {slc_meta_t} spec
+            auto slc = new slc_meta_t(layout);
             // dress it and return it
             return slc_pointer(slc);
         }),
@@ -72,7 +72,7 @@ flocor::py::slc_metadata(py::module & m)
         // the getter
         [](py::object) {
             // easy enough
-            return sizeof(slc_t::pixel_type);
+            return sizeof(slc_meta_t::pixel_type);
         },
         // the docstring
         "the memory footprint of an SLC pixel, in bytes");
@@ -81,7 +81,7 @@ flocor::py::slc_metadata(py::module & m)
     slc.def_property_readonly(
         "shape",
         // the getter
-        [](const slc_t & slc) { return slc.layout().shape(); },
+        [](const slc_meta_t & slc) { return slc.layout().shape(); },
         // the docstring
         "the shape of the spec");
 
