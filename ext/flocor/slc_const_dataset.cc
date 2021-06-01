b81ae244bed8c02ee73538068f7996f7880117dc
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-06-01 11:53:31 +0300

    flocor::products: added bindings for {slc_dataset} and {slc_const_dataset}

---
+++ ext/flocor/slc_const_dataset.cc
@@ -0,0 +1,175 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// externals
+#include "external.h"
+// namespace setup
+#include "forward.h"
+// libflocor
+#include <flocor/products.h>
+
+// type aliases
+namespace flocor::py {
+    // the dataset
+    using slc_const_dataset_t = flocor::products::slc_const_dataset_t;
+
+    // its parts
+    using value_t = slc_const_dataset_t::value_type;
+}
+
+// helpers
+namespace flocor::py {
+    // the constructor
+    inline auto slc_const_dataset_constructor(py::tuple, py::object)
+        -> unique_pointer<slc_const_dataset_t>;
+}
+
+
+// add bindings to SLC datasets
+void
+flocor::py::slc_const_dataset(py::module & m)
+{
+    // the SLC interface
+    py::class_<slc_const_dataset_t>(m, "SLCConstDataset")
+        // constructor
+        .def(
+            // the constructor wrapper
+            py::init([](py::tuple shape, py::object uri) {
+                return slc_const_dataset_constructor(shape, uri);
+            }),
+            // the signature
+            "shape"_a, "uri"_a)
+
+        // accessors
+        // sizes of things: number of pixels
+        .def_property_readonly(
+            "cells",
+            // the getter
+            &slc_const_dataset_t::cells,
+            // the docstring
+            "the number of pixels in the SLC")
+        // sizes of things: memory footprint
+        .def_property_readonly(
+            "bytes",
+            // the getter
+            &slc_const_dataset_t::bytes,
+            // the docstring
+            "the amount of memory occupied by this SLC, in bytes")
+        // access to the shape
+        .def_property_readonly(
+            "tile",
+            // the getter
+            [](const slc_const_dataset_t & slc) {
+                // get the shape
+                auto shape = slc.layout().shape();
+                // convert it to a tuple
+                auto pyShape = py::make_tuple(shape[0], shape[1]);
+                // get the tile from {pyre.grid}
+                auto pyFactory = py::module::import("pyre.grid").attr("tile");
+                // invoke it
+                auto pyTile = pyFactory("shape"_a = pyShape);
+                // and return it
+                return pyTile;
+            },
+            // the docstring
+            "the shape of the SLC")
+        // compute the range of values in the dataset
+        .def_property_readonly(
+            "range",
+            // the getter
+            [](const slc_const_dataset_t & slc) {
+                // the sampling fraction
+                auto skip = 1000;
+                // get the number of pixels
+                auto pixels = slc.cells();
+                // the normalization factor
+                auto samples = (1.0 * pixels) / skip;
+
+                // initialize the values; don't bother with square roots for now
+                auto av2 = std::norm(slc[0]);
+                // sample the dataset
+                for (auto i = 0; i < pixels; i += skip) {
+                    // accumulate the value
+                    av2 += std::norm(slc[i]) / samples;
+                }
+
+                //  get the variance
+                auto var = std::sqrt(av2);
+                // set a scale
+                auto scale = 5.0;
+
+                // build the answer and return
+                return std::pair(var / scale, scale * var);
+            },
+            //   the docstring
+            "compute the range of values in the dataset")
+
+        // metamethods
+        // data read access given an index
+        .def(
+            "__getitem__",
+            // convert the incoming tuple into an index and fetch the data
+            [](const slc_const_dataset_t & slc, py::tuple pyIdx) {
+                // type aliases
+                using index_t = slc_const_dataset_t::index_type;
+                using rank_t = slc_const_dataset_t::index_type::rank_type;
+                // make an index out of the python tuple
+                index_t idx { pyIdx[0].cast<rank_t>(), pyIdx[1].cast<rank_t>() };
+                // get the data and return it
+                return slc[idx];
+            },
+            // the signature
+            "index"_a,
+            // the docstring
+            "access the data at the given index")
+        // data read access given an offset
+        .def(
+            "__getitem__",
+            // delegate directly to the {slc_const_dataset_t}
+            [](const slc_const_dataset_t & slc, size_t offset) {
+                // easy enough
+                return slc[offset];
+            },
+            // the signature
+            "offset"_a,
+            // the docstring
+            "access the data at the given offset")
+        // done
+        ;
+
+    // all done
+    return;
+}
+
+
+// helper definitions
+auto
+flocor::py::slc_const_dataset_constructor(py::tuple pyShape, py::object pyURI)
+    -> unique_pointer<slc_const_dataset_t>
+{
+    // extract the shape
+    int lines = py::int_(pyShape[0]);
+    int samples = py::int_(pyShape[1]);
+
+    // make a shape
+    slc_const_dataset_t::shape_type shape { lines, samples };
+    // turn it into a layout
+    slc_const_dataset_t::layout_type layout { shape };
+    // make a product specification
+    slc_const_dataset_t::spec_type spec { layout };
+
+    // convert the path-like object into a string
+    // get {os.fspath}
+    auto fspath = py::module::import("os").attr("fspath");
+    // call it and convert its return value into a string
+    string_t filename = py::str(fspath(pyURI));
+
+    // build the product and return it
+    return std::unique_ptr<slc_const_dataset_t>(new slc_const_dataset_t(spec, filename));
+}
+
+
+// end of file
---
+++ ext/flocor/slc_dataset.cc
@@ -0,0 +1,153 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// externals
+#include "external.h"
+// namespace setup
+#include "forward.h"
+// libflocor
+#include <flocor/products.h>
+
+
+// type aliases
+namespace flocor::py {
+    using slc_dataset_t = flocor::products::slc_dataset_t;
+}
+
+// helpers
+namespace flocor::py {
+    // the constructor
+    inline auto slc_dataset_constructor(py::tuple, py::object, bool) -> unique_pointer<slc_dataset_t>;
+}
+
+
+// add bindings to SLC datasets
+void
+flocor::py::slc_dataset(py::module & m)
+{
+    // the SLC interface
+    py::class_<slc_dataset_t>(m, "SLCDataset")
+        // constructor
+        .def(
+            // the constructor wrapper
+            py::init([](py::tuple shape, py::object uri, bool create) {
+                // ask the helper
+                return slc_dataset_constructor(shape, uri, create);
+            }),
+            // the signature
+            "shape"_a, "uri"_a, "new"_a)
+
+        // accessors
+        // sizes of things: number of pixels
+        .def_property_readonly(
+            "cells",
+            // the getter
+            &slc_dataset_t::cells,
+            // the docstring
+            "the number of pixels in the SLC")
+        // sizes of things: memory footprint
+        .def_property_readonly(
+            "bytes",
+            // the getter
+            &slc_dataset_t::bytes,
+            // the docstring
+            "the amount of memory occupied by this SLC, in bytes")
+
+        // access to the shape
+        .def_property_readonly(
+            "tile",
+            // the getter
+            [](const slc_dataset_t & slc) {
+                // get the shape
+                auto shape = slc.layout().shape();
+                // convert it to a tuple
+                auto pyShape = py::make_tuple(shape[0], shape[1]);
+                // get the tile from {pyre.grid}
+                auto pyFactory = py::module::import("pyre.grid").attr("tile");
+                // invoke it
+                auto pyTile = pyFactory("shape"_a = pyShape);
+                // and return it
+                return pyTile;
+            },
+            // the docstring
+            "the shape of the SLC")
+
+        // metamethods
+        // data read access given an index
+        .def(
+            "__getitem__",
+            // convert the incoming tuple into an index and fetch the data
+            [](slc_dataset_t & slc, py::tuple pyIdx) -> slc_dataset_t::pixel_type & {
+                // type aliases
+                using index_t = slc_dataset_t::index_type;
+                using rank_t = slc_dataset_t::index_type::rank_type;
+                // make an index out of the python tuple
+                slc_dataset_t::index_type idx { pyIdx[0].cast<rank_t>(), pyIdx[1].cast<rank_t>() };
+                // get the data and return it
+                return slc[idx];
+            },
+            // the signature
+            "index"_a,
+            // the docstring
+            "access the data at the given index",
+            // grant write access
+            py::return_value_policy::reference)
+        // data read access given an offset
+        .def(
+            "__getitem__",
+            // delegate directly to the {slc_dataset_t}
+            [](slc_dataset_t & slc, size_t offset) -> slc_dataset_t::pixel_type & {
+                // easy enough
+                return slc[offset];
+            },
+            // the signature
+            "offset"_a,
+            // the docstring
+            "access the data at the given offset",
+            // grant write access
+            py::return_value_policy::reference)
+        // done
+        ;
+
+    // all done
+    return;
+}
+
+
+// helper definitions
+auto
+flocor::py::slc_dataset_constructor(py::tuple pyShape, py::object pyURI, bool create)
+    -> unique_pointer<slc_dataset_t>
+{
+    // extract the shape
+    int lines = py::int_(pyShape[0]);
+    int samples = py::int_(pyShape[1]);
+
+    // make a shape
+    slc_dataset_t::shape_type shape { lines, samples };
+    // turn it into a layout
+    slc_dataset_t::layout_type layout { shape };
+    // make a product specification
+    slc_dataset_t::spec_type spec { layout };
+
+    // convert the path-like object into a string
+    // get {os.fspath}
+    auto fspath = py::module::import("os").attr("fspath");
+    // call it and convert its return value into a string
+    string_t filename = py::str(fspath(pyURI));
+
+    // if we are supposed to create a new one
+    if (create) {
+        // build the product and return it
+        return std::unique_ptr<slc_dataset_t>(new slc_dataset_t(spec, filename, spec.cells()));
+    }
+
+    // otherwise, just open an existing one in read/write mode
+    return std::unique_ptr<slc_dataset_t>(new slc_dataset_t(spec, filename, true));
+}
+
+
+// end of file
