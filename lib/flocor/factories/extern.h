d06b9b3ac3822b0343e57679de6c5a1f75b61157
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-06-02 14:15:32 +0300

    flocor::factories: added local type aliases for {pyre::grid} parts

--- lib/flocor/factories/extern.h
+++ lib/flocor/factories/extern.h
@@ -7,12 +7,22 @@
 #if !defined(flocor_factories_extern_h)
 #define flocor_factories_extern_h
 
-// pull the products api
+
+// we need the product definitions
 #include "../products.h"
 
 
 // local aliases for external entities
 namespace flocor::factories {
+    // the canonical layout
+    template <int N>
+    using layout_t = pyre::grid::canonical_t<N>;
+
+    // the rasters have a 2d canonical layout
+    using layout2d_t = layout_t<2>;
+
+    // {covering} from {products}
+    using covering_t = products::covering_t;
 }
 
 
