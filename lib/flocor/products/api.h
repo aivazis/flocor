cded7f641ef9a5c0a66341cf7ba5939076dc790a
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-06-02 09:29:55 +0300

    flocor::products: added {dataset_t} type aliases

--- lib/flocor/products/api.h
+++ lib/flocor/products/api.h
@@ -10,12 +10,20 @@
 
 // publicly visible types
 namespace flocor::products {
+    // data payloads
+    // read/write
+    template <class specT>
+    using dataset_t = Raster<specT, false>;
+    // read only
+    template <class specT>
+    using const_dataset_t = Raster<specT, true>;
+
     // slc
     // the spec
     using slc_metadata_t = SLCMetadata;
     // the rasters
-    using slc_dataset_t = Raster<slc_metadata_t, false>;         // read/write
-    using slc_const_dataset_t = Raster<slc_metadata_t, true>;    // read-only
+    using slc_dataset_t = dataset_t<slc_metadata_t>;                // read/write
+    using slc_const_dataset_t = const_dataset_t<slc_metadata_t>;    // read-only
 
     // a covering is a read/write grid of pixel coordinates, stored on the heap
     using covering_t = pyre::grid::grid_t<layout_t<2>, heap_t<std::pair<int, int>>>;
